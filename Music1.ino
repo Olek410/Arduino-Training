124bc210e565c21f5f36ffb77b5dc01ade912e22
Olek410 <olek5622@gmail.com>
2017-11-09 15:24:54 -0600

    Add files via upload

---
+++ Music1.ino
@@ -0,0 +1,94 @@
+/* Melody
+ * (cleft) 2005 D. Cuartielles for K3
+ *
+ * This example uses a piezo speaker to play melodies.  It sends
+ * a square wave of the appropriate frequency to the piezo, generating
+ * the corresponding tone.
+ *
+ * The calculation of the tones is made following the mathematical
+ * operation:
+ *
+ *       timeHigh = period / 2 = 1 / (2 * toneFrequency)
+ *
+ * where the different tones are described as in the table:
+ *
+ * note 	frequency 	period 	timeHigh
+ * c 	        261 Hz 	        3830 	1915 	
+ * d 	        294 Hz 	        3400 	1700 	
+ * e 	        329 Hz 	        3038 	1519 	
+ * f 	        349 Hz 	        2864 	1432 	
+ * g 	        392 Hz 	        2550 	1275 	
+ * a 	        440 Hz 	        2272 	1136 	
+ * b 	        493 Hz 	        2028	1014	
+ * C	        523 Hz	        1912 	956
+ *
+ * http://www.arduino.cc/en/Tutorial/Melody
+ */
+  
+int speakerPin = 9;
+
+int length = 15; // the number of notes
+char notes[] = "ccggaagffeeddc "; // a space represents a rest
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+int tempo = 300;
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void setup() {
+  pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+ 
+  delay(beats[0] * tempo); // rest
+   
+      playNote(notes[0], beats[0] * tempo);
+  
+  delay(beats[7] * tempo); // rest
+   
+      playNote(notes[7], beats[7] * tempo);
+  
+  delay(beats[1] * tempo); // rest
+   
+      playNote(notes[1], beats[1] * tempo);
+  
+  delay(beats[6] * tempo); // rest
+   
+      playNote(notes[6], beats[6] * tempo);
+  
+  delay(beats[2] * tempo); // rest
+   
+      playNote(notes[2], beats[2] * tempo);
+  
+  delay(beats[5] * tempo); // rest
+   
+      playNote(notes[5], beats[5] * tempo);
+  
+  delay(beats[3] * tempo); // rest
+   
+      playNote(notes[3], beats[3] * tempo);
+  
+  delay(beats[4] * tempo); // rest
+   
+      playNote(notes[4], beats[4] * tempo);
+ 
+}
